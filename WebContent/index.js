5608d8534152d26344dde37a13ff676ca6ee9565
nishantgoyal880 <nishantgoyal880@gmail.com>
2018-02-27 21:08:43 +0530

    initial commit

---
+++ WebContent/index.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+function getdata(){
+	var search=document.getElementById("data").value;
+	var xmlHttp = new XMLHttpRequest();
+	
+	var url = "https://newsapi.org/v2/everything?q="+search+"&from=2018-02-26&sortBy=popularity&apiKey=22e9046d2cb54f22a3246226766ad815";
+	xmlHttp.open("GET",url, true);
+    xmlHttp.send();
+    xmlHttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            var myArr= JSON.parse(this.responseText);
+            var dataObj= JSON.stringify(myArr);
+     
+            
+            
+            
+            
+            for (i = 0; i < myArr.articles.length; i++) {
+                var html_code = "<b>Source</b> : "+myArr.articles[i].source.name+"</br> <b>Title</b> : "+myArr.articles[i].title+
+                "</br> <b>Description</b> : "+myArr.articles[i].description+ 
+                "</br><button id =\"button"+i+"\" onClick=\"addtofav("+myArr.articles[i].source.name+
+                ","+myArr.articles[i].title+","+myArr.articles[i].description+")\">Add to favourite</button>"+
+                " </br> </br> ";
+                document.getElementById('content').insertAdjacentHTML('afterend', html_code);
+            }
+        
+            
+        }
+    };
+ }
+
+
+function 
